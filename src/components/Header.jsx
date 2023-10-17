485d13c0a7579443d3a589f890533b4071584efa
Dipak Rajbhar <140643796+dipak12803@users.noreply.github.com>
2023-10-17 10:15:56 +0000

    Update 24 files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import { MdShoppingBasket, MdAdd, MdLogout } from "react-icons/md";
 import { motion } from "framer-motion";
-
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+
 import { app } from "../firebase.config";
 
-import Logo from "../img/logo.png";
+import Logo from "../img/logos.jpg";
 import Avatar from "../img/avatar.png";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../context/StateProvider";
 import { actionType } from "../context/reducer";
+
 
 const Header = () => {
   const firebaseAuth = getAuth(app);
@@ -34,6 +36,7 @@ const Header = () => {
     }
   };
 
+  
   const logout = () => {
     setIsMenu(false);
     localStorage.clear();
@@ -56,8 +59,8 @@ const Header = () => {
       {/* desktop & tablet */}
       <div className="hidden md:flex w-full h-full items-center justify-between">
         <Link to={"/"} className="flex items-center gap-2">
-          <img src={Logo} className="w-8 object-cover" alt="logo" />
-          <p className="text-headingColor text-xl font-bold"> City</p>
+          <img src={Logo} className="w-12 object-cover rounded" alt="logo" />
+          {/* <p className="text-headingColor text-xl font-bold"> City</p> */}
         </Link>
 
         <div className="flex items-center gap-8">
@@ -98,7 +101,7 @@ const Header = () => {
           <div className="relative">
             <motion.img
               whileTap={{ scale: 0.6 }}
-              src={user ? user.photoURL : Avatar}
+              src={user?user.photoURL:Avatar}
               className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
               alt="userprofile"
               onClick={login}
@@ -110,7 +113,7 @@ const Header = () => {
                 exit={{ opacity: 0, scale: 0.6 }}
                 className="w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-12 right-0"
               >
-                {user && user.email === "vetrivel.galaxy@gmail.com" && (
+                {user && user.email === "dipakrajbhar12803@gmail.com" && (
                   <Link to={"/createItem"}>
                     <p
                       className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base"
@@ -157,7 +160,7 @@ const Header = () => {
         <div className="relative">
           <motion.img
             whileTap={{ scale: 0.6 }}
-            src={user ? user.photoURL : Avatar}
+            src={user?user.photoURL:Avatar}
             className="w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl cursor-pointer rounded-full"
             alt="userprofile"
             onClick={login}
