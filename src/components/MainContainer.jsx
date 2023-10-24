84b3186242f3a9d335d8ddf0b2b9bcfa164191ab
dipak12803 <dipakrajbhar12803@gmail.com>
2023-10-24 12:52:49 +0530

    removed unnecessary images

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -20,7 +20,7 @@ const MainContainer = () => {
       <section className="w-full my-6">
         <div className="w-full flex items-center justify-between">
           <p className="text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100">
-            Our fresh & healthy fruits
+            Best Selling Items🔥
           </p>
 
           <div className="hidden md:flex gap-3 items-center">
